1c8dee3619aeb751ed8f2117d413db9e7c4e3504
nithyadurai87 <nithyadurai87@gmail.com>
2018-09-24 12:04:57 +0530

    Update 01_stats_data.py

--- 01_stats_data.py
+++ 01_stats_data.py
@@ -1,17 +1,13 @@
 import matplotlib.pyplot as plt 
-x=[[6],[8],[10],[14],[18]]
-y=[[7],[9],[13],[17.5],[18]]
+
+x=[[6],[8],[10],[14],[18],[21]]
+y=[[7],[9],[13],[17.5],[18],[24]]
+
 plt.figure()
-plt.title('Pizzapriceplottedagainstdiameter')
-plt.xlabel('Diameterininches')
-plt.ylabel('Priceindollars')
+plt.title('Pizza price statistics')
+plt.xlabel('Diameter (inches)')
+plt.ylabel('Price (dollars)')
 plt.plot(x,y,'.')
 plt.axis([0,25,0,25])
 plt.grid(True)
 plt.show()
-
-# AZure data bricks model development using spark M-Lib
-
-# NOX Model and Predictions
-
-# Inputs for NOX 
